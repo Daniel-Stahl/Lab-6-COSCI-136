d7ee2f189aee842037f41056865a4e7e6d9b00cd
Daniel Stahl <danny.stahl@gmail.com>
2020-04-18 19:21:08 -0700

    Initializing lab 5

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAB_5_STAHL_DANIEL
+//
+//  Created by Daniel Stahl on 4/18/20.
+//  Copyright © 2020 Daniel Stahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
